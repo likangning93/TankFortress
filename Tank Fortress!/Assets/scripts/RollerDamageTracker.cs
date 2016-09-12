b7d35a5654456ba12083cbf61e764e3a0f83077f
Kangning Li <likangning93@gmail.com>
2016-09-12 19:33:26 -0400

    added rolling damage

---
+++ Tank Fortress!/Assets/scripts/RollerDamageTracker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollerDamageTracker : MonoBehaviour {
+
+    public HealthTracker parentHealthTracker;
+    public float damageMultiplier = 1.0f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Roller" &&
+            !parentHealthTracker.damagedThisFrame)
+        {
+            parentHealthTracker.damagedThisFrame = true;
+            parentHealthTracker.health -= (int)(damageMultiplier * (float)collision.collider.GetComponent<damager>().damageDealt);
+            //print("damage from sub tracker roller collision");
+        }
+    }
+}
---
+++ Tank Fortress!/Assets/scripts/tank/RollerDamageModulator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollerDamageModulator : MonoBehaviour {
+
+    public damager rollerDamager;
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rollerDamager.damageDealt = (int) Mathf.Abs(rb2d.angularVelocity);
+	}
+}
