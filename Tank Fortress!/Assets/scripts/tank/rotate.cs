71b7083754b28402342d7f1c54a6c1ab1131ecd6
Kangning Li <likangning93@gmail.com>
2016-09-12 23:33:57 -0400

    working on respawn point

---
+++ Tank Fortress!/Assets/scripts/tank/rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+    public float dRotation = 5.0f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotation.y += dRotation;
+        transform.localRotation = Quaternion.Euler(rotation);
+	}
+}
