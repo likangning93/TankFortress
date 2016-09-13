354cd2753f617d15da13106b5196273565a49cb9
Kangning Li <likangning93@gmail.com>
2016-09-12 22:58:15 -0400

    added a cute little explode effect

---
+++ Tank Fortress!/Assets/scripts/explodeOnDestroy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explodeOnDestroy : MonoBehaviour {
+
+    public Transform explosionPrefab;
+    public float radius = 5.0f;
+    public int explosionCount = 10;
+
+    // http://answers.unity3d.com/questions/169656/instantiate-ondestroy.html
+    bool isQuitting = false;
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+    void OnDestroy()
+    {
+        if (!isQuitting)
+        {
+            spawnExplosions();
+        }
+    }
+
+	// Update is called once per frame
+    Vector3 offset = new Vector3();
+    void spawnExplosions()
+    {
+        Vector3 position = transform.position;
+
+        for (int i = 0; i < explosionCount; i++)
+        {
+            offset.x = 0.5f - Random.value * radius;
+            offset.y = 0.5f - Random.value * radius;
+            Transform explosionClone = Instantiate(explosionPrefab);
+            explosionClone.transform.position = transform.position + offset;
+            explosion explode = explosionClone.GetComponent<explosion>();
+            explode.delay = i * 3;
+        }
+	}
+}
--- Tank Fortress!/Assets/scripts/projectiles/explosion.cs
+++ Tank Fortress!/Assets/scripts/projectiles/explosion.cs
@@ -5,6 +5,7 @@ public class explosion : MonoBehaviour {
 
     public int lifeSpan = 30;
     public float maxScale = 10.0f;
+    public int delay = 0;
 
     int lifeRemaining;
     float scaleStep;
@@ -20,13 +21,24 @@ public class explosion : MonoBehaviour {
     Vector3 scaleScratch = new Vector3();
     void FixedUpdate()
     {
-        currentScale += scaleStep;
-        lifeRemaining--;
+        if (delay > 0)
+        {
+            delay--;
+            scaleScratch.x = 0.0f;
+            scaleScratch.y = 0.0f;
+            scaleScratch.z = 0.0f;
+            transform.localScale = scaleScratch;
+        }
+        else
+        {
+            currentScale += scaleStep;
+            lifeRemaining--;
 
-        scaleScratch.x = currentScale;
-        scaleScratch.y = currentScale;
-        scaleScratch.z = currentScale;
-        transform.localScale = scaleScratch;
+            scaleScratch.x = currentScale;
+            scaleScratch.y = currentScale;
+            scaleScratch.z = currentScale;
+            transform.localScale = scaleScratch;
+        }
     }
 	
 	// Update is called once per frame
